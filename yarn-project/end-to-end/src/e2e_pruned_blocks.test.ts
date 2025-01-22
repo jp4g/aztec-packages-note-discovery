AztecProtocol/aztec-packages

commit 708139d2a30dbafe38171e826ac7462c20d2c5d9

feat: process note logs in aztec-nr (#10651)
Closes https://github.com/AztecProtocol/aztec-packages/issues/9576

This PR offloads the later stages of note log processing from PXE into
aztec-nr. The plan is to take over at the decrypted log payload stage,
and later on expand scope to also include decryption and (eventually)
tagging indices management.

Update: this now works, with some caveats. I'll leave some comments here
re. current status in case anyone wants to take a look, and for me to
resume work once I'm back.

Contracts are now expected to have a `process_logs` function that will
be called during note syncing. This function is not yet being
autogenerated, but I did manually add it to SchnorrAccount and
TokenContract for local testing - it should be fairly easy to
autogenerate it.

PXE still performs tagging index synchronization, fetches all relevant
logs, decrypts them, and merges the public and private components when
they are partial note logs. This will continue to be this way for a
while: we'll tackle these problems in #10723 and #10724. However, past
this point we delegate work to the contract.[^1] The contract performs
nonce discovery and computes note hash and nullifier, and then calls a
new PXE oracle called `deliverNote`. PXE validates that the note hash
exists in the tree, and adds the note to its database. **Edit:** I now
updated this section to remove all of the old relevant TS code. We no
longer do nonce discovery in PXE.

With this first step, PXE no longer needs to know about note type ids,
which become exclusively an aztec-nr concept. It will continue to know
about storage slots, but only because we index by them. More importantly
however, this makes us quite ready to continue building on top of this
work in order to fully move the other parts of the stack (notably
decryption and partial notes) into the contract as well.

[^1]: As of right now we're still doing all of the work in PXE,

yarn-project/pxe/src/simulator_oracle/index.ts
@@ -1,14 +1,16 @@
 import {
   type AztecNode,
+  type FunctionCall,
   type InBlock,
   L1NotePayload,
   type L2Block,
   type L2BlockNumber,
   MerkleTreeId,
+  Note,
   type NoteStatus,
   type NullifierMembershipWitness,
   type PublicDataWitness,
-  type TxEffect,
+  TxHash,
   type TxScopedL2Log,
   getNonNullifiedL1ToL2MessageWitness,
 } from '@aztec/circuit-types';
@@ -18,15 +20,24 @@ import {
   type CompleteAddress,
   type ContractInstance,
   Fr,
-  type FunctionSelector,
+  FunctionSelector,
   IndexedTaggingSecret,
   type KeyValidationRequest,
   type L1_TO_L2_MSG_TREE_HEIGHT,
+  MAX_NOTE_HASHES_PER_TX,
+  PRIVATE_LOG_SIZE_IN_FIELDS,
   PrivateLog,
   computeAddressSecret,
   computeTaggingSecretPoint,
 } from '@aztec/circuits.js';
-import { type FunctionArtifact, getFunctionArtifact } from '@aztec/foundation/abi';
+import { computeUniqueNoteHash, siloNoteHash, siloNullifier } from '@aztec/circuits.js/hash';
+import {
+  type FunctionArtifact,
+  FunctionType,
+  NoteSelector,
+  encodeArguments,
+  getFunctionArtifact,
+} from '@aztec/foundation/abi';
 import { poseidon2Hash } from '@aztec/foundation/crypto';
 import { createLogger } from '@aztec/foundation/log';
 import { type KeyStore } from '@aztec/key-store';
@@ -37,10 +48,10 @@ import {
   type SimulationProvider,
 } from '@aztec/simulator/client';
 
-import { type ContractDataOracle } from '../contract_data_oracle/index.js';
+import { ContractDataOracle } from '../contract_data_oracle/index.js';
 import { type PxeDatabase } from '../database/index.js';
-import { type NoteDao } from '../database/note_dao.js';
-import { produceNoteDaos } from '../note_decryption_utils/produce_note_daos.js';
+import { NoteDao } from '../database/note_dao.js';
+import { getOrderedNoteItems } from '../note_decryption_utils/add_public_values_to_payload.js';
 import { getAcirSimulator } from '../simulator/index.js';
 import { WINDOW_HALF_SIZE, getIndexedTaggingSecretsForTheWindow, getInitialIndexesMap } from './tagging_utils.js';
 
@@ -431,6 +442,8 @@ export class SimulatorOracle implements DBOracle {
     maxBlockNumber: number,
     scopes?: AztecAddress[],
   ): Promise<Map<string, TxScopedL2Log[]>> {
+    this.log.verbose('Searching for tagged logs', { contract: contractAddress });
+
     // Ideally this algorithm would be implemented in noir, exposing its building blocks as oracles.
     // However it is impossible at the moment due to the language not supporting nested slices.
     // This nesting is necessary because for a given set of tags we don't
@@ -568,10 +581,9 @@ export class SimulatorOracle implements DBOracle {
    * Decrypts logs tagged for a recipient and returns them.
    * @param scopedLogs - The logs to decrypt.
    * @param recipient - The recipient of the logs.
-   * @param simulator - The simulator to use for decryption.
    * @returns The decrypted notes.
    */
-  async #decryptTaggedLogs(scopedLogs: TxScopedL2Log[], recipient: AztecAddress, simulator?: AcirSimulator) {
+  async #decryptTaggedLogs(scopedLogs: TxScopedL2Log[], recipient: AztecAddress) {
     const recipientCompleteAddress = await this.getCompleteAddress(recipient);
     const ivskM = await this.keyStore.getMasterSecretKey(
       recipientCompleteAddress.publicKeys.masterIncomingViewingPublicKey,
@@ -581,56 +593,29 @@ export class SimulatorOracle implements DBOracle {
     // Since we could have notes with the same index for different txs, we need
     // to keep track of them scoping by txHash
     const excludedIndices: Map<string, Set<number>> = new Map();
-    const notes: NoteDao[] = [];
-
-    const txEffectsCache = new Map<string, InBlock<TxEffect> | undefined>();
+    const decrypted = [];
 
     for (const scopedLog of scopedLogs) {
-      const notePayload = scopedLog.isFromPublic
+      const payload = scopedLog.isFromPublic
         ? L1NotePayload.decryptAsIncomingFromPublic(scopedLog.logData, addressSecret)
         : L1NotePayload.decryptAsIncoming(PrivateLog.fromBuffer(scopedLog.logData), addressSecret);
 
-      if (notePayload) {
-        const payload = notePayload;
-
-        const txEffect =
-          txEffectsCache.get(scopedLog.txHash.toString()) ?? (await this.aztecNode.getTxEffect(scopedLog.txHash));
-
-        if (!txEffect) {
-          this.log.warn(`No tx effect found for ${scopedLog.txHash} while decrypting tagged logs`);
-          continue;
-        }
-
-        txEffectsCache.set(scopedLog.txHash.toString(), txEffect);
-
-        if (!excludedIndices.has(scopedLog.txHash.toString())) {
-          excludedIndices.set(scopedLog.txHash.toString(), new Set());
-        }
-        const { note } = await produceNoteDaos(
-          // I don't like this at all, but we need a simulator to run `computeNoteHashAndOptionallyANullifier`. This generates
-          // a chicken-and-egg problem due to this oracle requiring a simulator, which in turn requires this oracle. Furthermore, since jest doesn't allow
-          // mocking ESM exports, we have to pollute the method even more by providing a simulator parameter so tests can inject a fake one.
-          simulator ??
-            getAcirSimulator(this.db, this.aztecNode, this.keyStore, this.simulationProvider, this.contractDataOracle),
-          this.db,
-          notePayload ? recipient.toAddressPoint() : undefined,
-          payload!,
-          txEffect.data.txHash,
-          txEffect.data.nullifiers[0],
-          txEffect.l2BlockNumber,
-          txEffect.l2BlockHash,
-          txEffect.data.noteHashes,
-          scopedLog.dataStartIndexForTx,
-          excludedIndices.get(scopedLog.txHash.toString())!,
-          this.log,
-        );
-
-        if (note) {
-          notes.push(note);
-        }
+      if (!payload) {
+        this.log.verbose('Unable to decrypt log');
+        continue;
       }
+
+      if (!excludedIndices.has(scopedLog.txHash.toString())) {
+        excludedIndices.set(scopedLog.txHash.toString(), new Set());
+      }
+
+      const note = await getOrderedNoteItems(this.db, payload);
+      const plaintext = [payload.storageSlot, payload.noteTypeId.toField(), ...note.items];
+
+      decrypted.push({ plaintext, txHash: scopedLog.txHash, contractAddress: payload.contractAddress });
     }
-    return { notes };
+
+    return decrypted;
   }
 
   /**
@@ -643,20 +628,68 @@ export class SimulatorOracle implements DBOracle {
     recipient: AztecAddress,
     simulator?: AcirSimulator,
   ): Promise<void> {
-    const { notes } = await this.#decryptTaggedLogs(logs, recipient, simulator);
-    if (notes.length) {
-      await this.db.addNotes(notes, recipient);
-      notes.forEach(noteDao => {
-        this.log.verbose(`Added incoming note for contract ${noteDao.contractAddress} at slot ${noteDao.storageSlot}`, {
-          contract: noteDao.contractAddress,
-          slot: noteDao.storageSlot,
-          nullifier: noteDao.siloedNullifier.toString(),
-        });
-      });
+    const decryptedLogs = await this.#decryptTaggedLogs(logs, recipient);
+
+    // We've produced the full NoteDao, which we'd be able to simply insert into the database. However, this is
+    // only a temporary measure as we migrate from the PXE-driven discovery into the new contract-driven approach. We
+    // discard most of the work done up to this point and reconstruct the note plaintext to then hand over to the
+    // contract for further processing.
+    for (const decryptedLog of decryptedLogs) {
+      // Log processing requires the note hashes in the tx in which the note was created. We are now assuming that the
+      // note was included in the same block in which the log was delivered - note that partial notes will not work this
+      // way.
+      const txEffect = await this.aztecNode.getTxEffect(decryptedLog.txHash);
+      if (!txEffect) {
+        throw new Error(`Could not find tx effect for tx hash ${decryptedLog.txHash}`);
+      }
+
+      // This will trigger calls to the deliverNote oracle
+      await this.callProcessLog(
+        decryptedLog.contractAddress,
+        decryptedLog.plaintext,
+        decryptedLog.txHash,
+        txEffect.data.noteHashes,
+        txEffect.data.nullifiers[0],
+        recipient,
+        simulator,
+      );
     }
+    return;
+  }
+
+  // Called when notes are delivered, usually as a result to a call to the process_log contract function
+  public async deliverNote(
+    contractAddress: AztecAddress,
+    storageSlot: Fr,
+    nonce: Fr,
+    content: Fr[],
+    noteHash: Fr,
+    nullifier: Fr,
+    txHash: Fr,
+    recipient: AztecAddress,
+  ): Promise<void> {
+    const noteDao = await this.produceNoteDao(
+      contractAddress,
+      storageSlot,
+      nonce,
+      content,
+      noteHash,
+      nullifier,
+      txHash,
+      recipient,
+    );
+
+    await this.db.addNotes([noteDao], recipient);
+    this.log.verbose('Added note', {
+      contract: noteDao.contractAddress,
+      slot: noteDao.storageSlot,
+      nullifier: noteDao.siloedNullifier.toString,
+    });
   }
 
   public async removeNullifiedNotes(contractAddress: AztecAddress) {
+    this.log.verbose('Removing nullified notes', { contract: contractAddress });
+
     for (const recipient of await this.keyStore.getAccounts()) {
       const currentNotesForRecipient = await this.db.getNotes({ contractAddress, owner: recipient });
       const nullifiersToCheck = currentNotesForRecipient.map(note => note.siloedNullifier);
@@ -681,6 +714,96 @@ export class SimulatorOracle implements DBOracle {
     }
   }
 
+  async produceNoteDao(
+    contractAddress: AztecAddress,
+    storageSlot: Fr,
+    nonce: Fr,
+    content: Fr[],
+    noteHash: Fr,
+    nullifier: Fr,
+    txHash: Fr,
+    recipient: AztecAddress,
+  ): Promise<NoteDao> {
+    const receipt = await this.aztecNode.getTxReceipt(new TxHash(txHash));
+    if (receipt === undefined) {
+      throw new Error(`Failed to fetch tx receipt for tx hash ${txHash} when searching for note hashes`);
+    }
+    const { blockNumber, blockHash } = receipt;
+
+    const uniqueNoteHash = computeUniqueNoteHash(nonce, siloNoteHash(contractAddress, noteHash));
+    const siloedNullifier = siloNullifier(contractAddress, nullifier);
+
+    const uniqueNoteHashTreeIndex = (
+      await this.aztecNode.findLeavesIndexes(blockNumber!, MerkleTreeId.NOTE_HASH_TREE, [uniqueNoteHash])
+    )[0];
+    if (uniqueNoteHashTreeIndex === undefined) {
+      throw new Error(
+        `Note hash ${noteHash} (uniqued as ${uniqueNoteHash}) is not present on the tree at block ${blockNumber} (from tx ${txHash})`,
+      );
+    }
+
+    return new NoteDao(
+      new Note(content),
+      contractAddress,
+      storageSlot,
+      nonce,
+      noteHash,
+      siloedNullifier,
+      new TxHash(txHash),
+      blockNumber!,
+      blockHash!.toString(),
+      uniqueNoteHashTreeIndex,
+      recipient.toAddressPoint(),
+      NoteSelector.empty(), // todo: remove
+    );
+  }
+
+  async callProcessLog(
+    contractAddress: AztecAddress,
+    logPlaintext: Fr[],
+    txHash: TxHash,
+    noteHashes: Fr[],
+    firstNullifier: Fr,
+    recipient: AztecAddress,
+    simulator?: AcirSimulator,
+  ) {
+    const artifact: FunctionArtifact | undefined = await new ContractDataOracle(this.db).getFunctionArtifactByName(
+      contractAddress,
+      'process_log',
+    );
+    if (!artifact) {
+      throw new Error(
+        `Mandatory implementation of "process_log" missing in noir contract ${contractAddress.toString()}.`,
+      );
+    }
+
+    const execRequest: FunctionCall = {
+      name: artifact.name,
+      to: contractAddress,
+      selector: FunctionSelector.fromNameAndParameters(artifact),
+      type: FunctionType.UNCONSTRAINED,
+      isStatic: artifact.isStatic,
+      args: encodeArguments(artifact, [
+        toBoundedVec(logPlaintext, PRIVATE_LOG_SIZE_IN_FIELDS),
+        txHash.toString(),
+        toBoundedVec(noteHashes, MAX_NOTE_HASHES_PER_TX),
+        firstNullifier,
+        recipient,
+      ]),
+      returnTypes: artifact.returnTypes,
+    };
+
+    await (
+      simulator ??
+      getAcirSimulator(this.db, this.aztecNode, this.keyStore, this.simulationProvider, this.contractDataOracle)
+    ).runUnconstrained(
+      execRequest,
+      artifact,
+      contractAddress,
+      [], // empty scope as this call should not require access to private information
+    );
+  }
+
   /**
    * Used by contracts during execution to store arbitrary data in the local PXE database. The data is siloed/scoped
    * to a specific `contract`.
@@ -702,4 +825,8 @@ export class SimulatorOracle implements DBOracle {
   load(contract: AztecAddress, key: Fr): Promise<Fr[] | null> {
     return this.db.load(contract, key);
   }
+}
+
+function toBoundedVec(array: Fr[], maxLength: number) {
+  return { storage: array.concat(Array(maxLength - array.length).fill(new Fr(0))), len: array.length };
 }

yarn-project/pxe/src/note_decryption_utils/index.ts
@@ -1,2 +1,0 @@
-export { produceNoteDaos } from './produce_note_daos.js';
-export { NoteInfo } from './brute_force_note_info.js';

jp4g/aztec-packages-note-discovery

commit 1da6b5b9da7b5c335a2a3f49602a1b403d9b71a2

fix: prevent PXE from making historical queries during note discovery (#11406)
This fixes an issue introduced in
https://github.com/AztecProtocol/aztec-packages/pull/10651 which
resulted in PXE querying note hash tree indexes at the block number in
which they were created, accidentally resulting in historical queries
that failed in long-running environments such as masternet, but none of
the e2e tests.

I addressed this by querying at the latest locally synced block number
instead, which in practice should be the tip of the chain since we sync
right before simulations. I explicitly avoided using `'latest'` to
prevent adding notes that don't exist in the locally synced state, since
a) that's not properly supported at the moment, b) such an instance
would likely be indicative of a bug in note discovery, and c) we
eventually want to make queries based on the block _hash_ so as to
detect network reorgs and fail loudly.

I also created a new e2e test which creates note a note in an old block,
forces it to be pruned, and then has PXE discover and use that note.
Triggering this bug required creating a note and then mining upwards of
64 blocks on top while having proving enabled, which we only ever did in
live networks.

scripts/ci/get_e2e_jobs.sh
@@ -35,6 +35,7 @@ allow_list=(
   "e2e_max_block_number"
   "e2e_nested_contract"
   "e2e_ordering"
+  "e2e_pruned_blocks"
   "e2e_static_calls"
   "integration_l1_publisher"
   "e2e_cheat_codes"

yarn-project/end-to-end/scripts/e2e_test_config.yml
@@ -63,6 +63,7 @@ tests:
     test_path: 'e2e_prover/full.test.ts'
     env:
       HARDWARE_CONCURRENCY: '32'
+  e2e_pruned_blocks: {}
   e2e_public_testnet: {}
   e2e_pxe:
     use_compose: true

yarn-project/pxe/src/simulator_oracle/index.ts
@@ -743,21 +743,30 @@ export class SimulatorOracle implements DBOracle {
     txHash: Fr,
     recipient: AztecAddress,
   ): Promise<NoteDao> {
+    // We need to validate that the note does indeed exist in the world state to avoid adding notes that are then
+    // impossible to prove.
+
     const receipt = await this.aztecNode.getTxReceipt(new TxHash(txHash));
     if (receipt === undefined) {
       throw new Error(`Failed to fetch tx receipt for tx hash ${txHash} when searching for note hashes`);
     }
-    const { blockNumber, blockHash } = receipt;
 
+    // Siloed and unique hashes are computed by us instead of relying on values sent by the contract to make sure
+    // we're not e.g. storing notes that belong to some other contract, which would constitute a security breach.
     const uniqueNoteHash = computeUniqueNoteHash(nonce, siloNoteHash(contractAddress, noteHash));
     const siloedNullifier = siloNullifier(contractAddress, nullifier);
 
+    // We store notes by their index in the global note hash tree, which has the convenient side effect of validating
+    // note existence in said tree. Note that while this is technically a historical query, we perform it at the latest
+    // locally synced block number which *should* be recent enough to be available. We avoid querying at 'latest' since
+    // we want to avoid accidentally processing notes that only exist ahead in time of the locally synced state.
+    const syncedBlockNumber = await this.db.getBlockNumber();
     const uniqueNoteHashTreeIndex = (
-      await this.aztecNode.findLeavesIndexes(blockNumber!, MerkleTreeId.NOTE_HASH_TREE, [uniqueNoteHash])
+      await this.aztecNode.findLeavesIndexes(syncedBlockNumber!, MerkleTreeId.NOTE_HASH_TREE, [uniqueNoteHash])
     )[0];
     if (uniqueNoteHashTreeIndex === undefined) {
       throw new Error(
-        `Note hash ${noteHash} (uniqued as ${uniqueNoteHash}) is not present on the tree at block ${blockNumber} (from tx ${txHash})`,
+        `Note hash ${noteHash} (uniqued as ${uniqueNoteHash}) is not present on the tree at block ${syncedBlockNumber} (from tx ${txHash})`,
       );
     }
 
@@ -769,8 +778,8 @@ export class SimulatorOracle implements DBOracle {
       noteHash,
       siloedNullifier,
       new TxHash(txHash),
-      blockNumber!,
-      blockHash!.toString(),
+      receipt.blockNumber!,
+      receipt.blockHash!.toString(),
       uniqueNoteHashTreeIndex,
       await recipient.toAddressPoint(),
       NoteSelector.empty(), // todo: remove

yarn-project/end-to-end/src/e2e_pruned_blocks.test.ts
@@ -1,0 +1,115 @@
+import {
+  type AccountWallet,
+  type AztecAddress,
+  type AztecNode,
+  type Logger,
+  MerkleTreeId,
+  type Wallet,
+  sleep,
+} from '@aztec/aztec.js';
+import { type CheatCodes } from '@aztec/aztec.js/utils';
+import { TokenContract } from '@aztec/noir-contracts.js/Token';
+
+import { setup } from './fixtures/utils.js';
+
+// Tests PXE interacting with a node that has pruned relevant blocks, preventing usage of the archive API (which PXE
+// should not rely on).
+describe('e2e_pruned_blocks', () => {
+  let logger: Logger;
+  let teardown: () => Promise<void>;
+
+  let aztecNode: AztecNode;
+  let cheatCodes: CheatCodes;
+
+  let wallets: AccountWallet[];
+
+  let adminWallet: Wallet;
+  let senderWallet: Wallet;
+
+  let admin: AztecAddress;
+  let sender: AztecAddress;
+  let recipient: AztecAddress;
+
+  let token: TokenContract;
+
+  const MINT_AMOUNT = 1000n;
+
+  // Don't make this value too high since we need to mine this number of empty blocks, which is relatively slow.
+  const WORLD_STATE_BLOCK_HISTORY = 2;
+  const WORLD_STATE_CHECK_INTERVAL_MS = 300;
+
+  beforeAll(async () => {
+    ({ aztecNode, cheatCodes, logger, teardown, wallets } = await setup(3, {
+      worldStateBlockHistory: WORLD_STATE_BLOCK_HISTORY,
+      worldStateBlockCheckIntervalMS: WORLD_STATE_CHECK_INTERVAL_MS,
+    }));
+
+    [adminWallet, senderWallet] = wallets;
+    [admin, sender, recipient] = wallets.map(a => a.getAddress());
+
+    token = await TokenContract.deploy(adminWallet, admin, 'TEST', '$TST', 18).send().deployed();
+    logger.info(`L2 token contract deployed at ${token.address}`);
+  });
+
+  afterAll(() => teardown());
+
+  async function mineBlocks(blocks: number): Promise<void> {
+    // There's currently no cheatcode for mining blocks so we just create a couple dummy ones by calling a view function
+    for (let i = 0; i < blocks; i++) {
+      await token.methods.private_get_name().send().wait();
+    }
+  }
+
+  it('can discover and use notes created in both pruned and available blocks', async () => {
+    // This is the only test in this suite so it doesn't seem worthwhile to worry too much about reusable setup etc. For
+    // simplicity's sake I just did the entire thing here.
+
+    // We are going to mint two notes for the sender, each for half of a total amount, and then have the sender combine
+    // both in a transfer to the recipient. The catch is that enough blocks will be mined between the first and second
+    // mint transaction that the node will drop the block corresponding to the first mint, resulting in errors if PXE
+    // tried to access any historical information related to it (which it shouldn't).
+
+    const firstMintReceipt = await token
+      .withWallet(adminWallet)
+      .methods.mint_to_private(admin, sender, MINT_AMOUNT / 2n)
+      .send()
+      .wait();
+    const firstMintTxEffect = await aztecNode.getTxEffect(firstMintReceipt.txHash);
+
+    // mint_to_private should create just one new note with the minted amount
+    expect(firstMintTxEffect?.data.noteHashes.length).toEqual(1);
+    const mintedNote = firstMintTxEffect?.data.noteHashes[0];
+
+    // We now make a historical query for the leaf index at the block number in which this first note was created and
+    // check that we get a valid result, which indirectly means that the queried block has not yet been pruned.
+    expect(
+      (await aztecNode.findLeavesIndexes(firstMintReceipt.blockNumber!, MerkleTreeId.NOTE_HASH_TREE, [mintedNote!]))[0],
+    ).toBeGreaterThan(0);
+
+    // We now mine dummy blocks, mark them as proven and wait for the node to process them, which should result in older
+    // blocks (notably the one with the minted note) being pruned.
+    await mineBlocks(WORLD_STATE_BLOCK_HISTORY);
+    await cheatCodes.rollup.markAsProven();
+    await sleep(WORLD_STATE_CHECK_INTERVAL_MS * 2);
+
+    // The same historical query we performed before should now fail since this block is not available anymore.
+    await expect(
+      aztecNode.findLeavesIndexes(firstMintReceipt.blockNumber!, MerkleTreeId.NOTE_HASH_TREE, [mintedNote!]),
+    ).rejects.toThrow('Unable to find leaf');
+
+    // We've completed the setup we were interested in, and can now simply mint the second half of the amount, transfer
+    // the full amount to the recipient (which will require the sender to discover and prove both the old and new notes)
+    // and check that everything worked as expected.
+
+    await token
+      .withWallet(adminWallet)
+      .methods.mint_to_private(admin, sender, MINT_AMOUNT / 2n)
+      .send()
+      .wait();
+
+    await token.withWallet(senderWallet).methods.transfer(recipient, MINT_AMOUNT).send().wait();
+
+    expect(await token.methods.balance_of_private(recipient).simulate()).toEqual(MINT_AMOUNT);
+    expect(await token.methods.balance_of_private(sender).simulate()).toEqual(0n);
+  });
+});