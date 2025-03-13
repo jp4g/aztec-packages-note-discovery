AztecProtocol/aztec-packages

commit 6345158022a55ea1e0ccdd874d12bbcb39d3c639

feat: remove public bytecode from ts ContractArtifact (#12653)
After discussions from #12081 this PR:

- Removes public bytecode from the ts `ContractArtifact` class (it is
not required and carries around sometimes very large bytecode)
- Removes optional `aztecNrVersion`
- Updates `artifact_hash` documentation

playground/src/components/contract/components/deployContractDialog.tsx
@@ -14,8 +14,8 @@ import { css } from '@mui/styled-engine';
 import { useContext, useEffect, useState } from 'react';
 import {
   type ContractArtifact,
-  type FunctionArtifact,
   encodeArguments,
+  type FunctionAbi,
   getDefaultInitializer,
   getInitializer,
 } from '@aztec/stdlib/abi';
@@ -40,7 +40,7 @@ export function DeployContractDialog({
   onClose: (contract?: ContractInstanceWithAddress, alias?: string) => void;
 }) {
   const [alias, setAlias] = useState('');
-  const [initializer, setInitializer] = useState<FunctionArtifact>(null);
+  const [initializer, setInitializer] = useState<FunctionAbi>(null);
   const [parameters, setParameters] = useState([]);
   const [deploying, setDeploying] = useState(false);
   const { wallet, setLogsOpen } = useContext(AztecContext);
@@ -63,13 +63,7 @@ export function DeployContractDialog({
     setDeploying(true);
     setLogsOpen(true);
 
-    const nodeInfo = await wallet.getNodeInfo();
-    const expectedAztecNrVersion = `v${nodeInfo.nodeVersion}`;
-    if (contractArtifact.aztecNrVersion && contractArtifact.aztecNrVersion !== expectedAztecNrVersion) {
-      throw new Error(
-        `Contract was compiled with a different version of Aztec.nr: ${contractArtifact.aztecNrVersion}. Consider updating Aztec.nr to ${expectedAztecNrVersion}`,
-      );
-    }
+    // TODO(#12081): Add contractArtifact.noirVersion and check here (via Noir.lock)?
 
     const deployer = new ContractDeployer(contractArtifact, wallet, PublicKeys.default(), initializer?.name);
 

yarn-project/stdlib/src/abi/contract_artifact.ts
@@ -9,6 +9,7 @@ import {
   ContractArtifactSchema,
   type ContractNote,
   type FieldLayout,
+  type FunctionAbi,
   type FunctionArtifact,
   FunctionType,
   type IntegerValue,
@@ -44,6 +45,7 @@ export function contractArtifactFromBuffer(buffer: Buffer): Promise<ContractArti
 
 /**
  * Gets nargo build output and returns a valid contract artifact instance.
+ * Does not include public bytecode, apart from the public_dispatch function.
  * @param input - Input object as generated by nargo compile.
  * @returns A valid contract artifact instance.
  */
@@ -52,6 +54,16 @@ export function loadContractArtifact(input: NoirCompiledContract): ContractArtif
     return input;
   }
   return generateContractArtifact(input);
+}
+
+/**
+ * Gets nargo build output and returns a valid contract artifact instance.
+ * Differs from loadContractArtifact() by retaining all bytecode.
+ * @param input - Input object as generated by nargo compile.
+ * @returns A valid contract artifact instance.
+ */
+export function loadContractArtifactForPublic(input: NoirCompiledContract): ContractArtifact {
+  return generateContractArtifactForPublic(input);
 }
 
 /**
@@ -79,6 +91,11 @@ function isContractArtifact(input: any): input is ContractArtifact {
     if (typeof fn.functionType !== 'string') {
       return false;
     }
+    if (!retainBytecode(fn) && fn.bytecode.length) {
+      // We want to remove the bytecode of public fns (apart from the dispatch fn) to save space
+      // If the input is private-only, we don't need to use generateContractArtifact() below
+      return false;
+    }
   }
   return true;
 }
@@ -103,15 +120,21 @@ function generateFunctionParameter(param: NoirCompiledContractFunctionParameter)
 type NoirCompiledContractFunction = NoirCompiledContract['functions'][number];
 
 /**
- * Generates a function build artifact.
+ * Returns true if we should retain bytecode
+ */
+export function retainBytecode(input: NoirCompiledContractFunction | FunctionArtifact): boolean {
+  const functionType =
+    (input as FunctionArtifact).functionType ?? getFunctionType(input as NoirCompiledContractFunction);
+  return functionType !== FunctionType.PUBLIC || input.name == 'public_dispatch';
+}
+
+/**
+ * Generates a function abi.
  * @param fn - Noir function entry.
  * @param contract - Parent contract.
- * @returns Function artifact.
+ * @returns Function abi.
  */
-function generateFunctionArtifact(
-  fn: NoirCompiledContractFunction,
-  contract: NoirCompiledContract,
-): Omit<FunctionArtifact, 'bytecode'> & { bytecode: string } {
+function generateFunctionAbi(fn: NoirCompiledContractFunction, contract: NoirCompiledContract): FunctionAbi {
   if (fn.custom_attributes === undefined) {
     throw new Error(
       `No custom attributes found for contract function ${fn.name}. Try rebuilding the contract with the latest nargo version.`,
@@ -156,10 +179,26 @@ function generateFunctionArtifact(
     isInitializer: fn.custom_attributes.includes(AZTEC_INITIALIZER_ATTRIBUTE),
     parameters,
     returnTypes,
-    bytecode: fn.bytecode,
-    debugSymbols: fn.debug_symbols,
     errorTypes: fn.abi.error_types,
     ...(fn.assert_messages ? { assertMessages: fn.assert_messages } : undefined),
+  };
+}
+
+/**
+ * Generates a function build artifact.
+ * @param fn - Noir function entry.
+ * @param contract - Parent contract.
+ * @returns Function artifact.
+ */
+function generateFunctionArtifact(
+  fn: NoirCompiledContractFunction,
+  contract: NoirCompiledContract,
+): Omit<FunctionArtifact, 'bytecode'> & { bytecode: string } {
+  const abi = generateFunctionAbi(fn, contract);
+  return {
+    ...abi,
+    bytecode: fn.bytecode,
+    debugSymbols: fn.debug_symbols,
     ...(fn.verification_key ? { verificationKey: fn.verification_key } : undefined),
   };
 }
@@ -274,19 +313,46 @@ function getNoteTypes(input: NoirCompiledContract) {
 
 /**
  * Given a Nargo output generates an Aztec-compatible contract artifact.
+ * Does not include public bytecode, apart from the public_dispatch function.
  * @param compiled - Noir build output.
  * @returns Aztec contract build artifact.
  */
-function generateContractArtifact(contract: NoirCompiledContract, aztecNrVersion?: string): ContractArtifact {
+function generateContractArtifact(contract: NoirCompiledContract): ContractArtifact {
   try {
     return ContractArtifactSchema.parse({
       name: contract.name,
-      functions: contract.functions.map(f => generateFunctionArtifact(f, contract)),
+      functions: contract.functions.filter(f => retainBytecode(f)).map(f => generateFunctionArtifact(f, contract)),
+      nonDispatchPublicFunctions: contract.functions
+        .filter(f => !retainBytecode(f))
+        .map(f => generateFunctionAbi(f, contract)),
       outputs: contract.outputs,
       storageLayout: getStorageLayout(contract),
       notes: getNoteTypes(contract),
       fileMap: contract.file_map,
-      ...(aztecNrVersion ? { aztecNrVersion } : {}),
+    });
+  } catch (err) {
+    throw new Error(`Could not generate contract artifact for ${contract.name}: ${err}`);
+  }
+}
+
+/**
+ * Given a Nargo output generates an Aztec-compatible contract artifact.
+ * Retains all public bytecode.
+ * @param compiled - Noir build output.
+ * @returns Aztec contract build artifact.
+ */
+function generateContractArtifactForPublic(contract: NoirCompiledContract): ContractArtifact {
+  try {
+    return ContractArtifactSchema.parse({
+      name: contract.name,
+      functions: contract.functions.map(f => generateFunctionArtifact(f, contract)),
+      nonDispatchPublicFunctions: contract.functions
+        .filter(f => !retainBytecode(f))
+        .map(f => generateFunctionAbi(f, contract)),
+      outputs: contract.outputs,
+      storageLayout: getStorageLayout(contract),
+      notes: getNoteTypes(contract),
+      fileMap: contract.file_map,
     });
   } catch (err) {
     throw new Error(`Could not generate contract artifact for ${contract.name}: ${err}`);

playground/src/components/contract/components/registerContractDialog.tsx
@@ -41,13 +41,7 @@ export function RegisterContractDialog({
   const register = async () => {
     setRegistering(true);
 
-    const nodeInfo = await wallet.getNodeInfo();
-    const expectedAztecNrVersion = `v${nodeInfo.nodeVersion}`;
-    if (contractArtifact.aztecNrVersion && contractArtifact.aztecNrVersion !== expectedAztecNrVersion) {
-      throw new Error(
-        `Contract was compiled with a different version of Aztec.nr: ${contractArtifact.aztecNrVersion}. Consider updating Aztec.nr to ${expectedAztecNrVersion}`,
-      );
-    }
+    // TODO(#12081): Add contractArtifact.noirVersion and check here (via Noir.lock)?
 
     const contractInstance = await node.getContract(AztecAddress.fromString(address));
 

yarn-project/archiver/src/archiver/kv_archiver_store/contract_class_store.ts
@@ -97,6 +97,7 @@ function serializeContractClassPublic(contractClass: Omit<ContractClassPublicWit
     numToUInt8(contractClass.version),
     contractClass.artifactHash,
     contractClass.publicFunctions.length,
+    // TODO(#8985): The below should only contain the public dispatch function, and no others
     contractClass.publicFunctions?.map(f => serializeToBuffer(f.selector, f.bytecode.length, f.bytecode)) ?? [],
     contractClass.privateFunctions.length,
     contractClass.privateFunctions.map(serializePrivateFunction),

yarn-project/aztec.js/src/api/abi.ts
@@ -1,7 +1,9 @@
 export {
   type ContractArtifact,
   type FunctionArtifact,
+  type FunctionAbi,
   EventSelector,
+  FunctionType,
   FunctionSelector,
   FunctionCall,
   NoteSelector,
@@ -15,6 +17,8 @@ export {
   isWrappedFieldStruct,
   isFunctionSelectorStruct,
   loadContractArtifact,
+  loadContractArtifactForPublic,
+  getAllFunctionAbis,
   contractArtifactToBuffer,
   contractArtifactFromBuffer,
 } from '@aztec/stdlib/abi';

yarn-project/aztec.js/src/contract/checker.test.ts
@@ -139,6 +139,7 @@ describe('abiChecker', () => {
               },
             },
           ],
+          isInitializer: true,
         },
       ],
     };
@@ -170,6 +171,7 @@ describe('abiChecker', () => {
               },
             },
           ],
+          isInitializer: true,
         },
       ],
     };

yarn-project/aztec.js/src/contract/checker.ts
@@ -1,4 +1,10 @@
-import type { AbiType, BasicType, ContractArtifact, StructType } from '@aztec/stdlib/abi';
+import {
+  type AbiType,
+  type BasicType,
+  type ContractArtifact,
+  type StructType,
+  getDefaultInitializer,
+} from '@aztec/stdlib/abi';
 
 /**
  * Represents a type derived from input type T with the 'kind' property removed.
@@ -38,8 +44,7 @@ export function abiChecker(artifact: ContractArtifact) {
     });
   });
 
-  // TODO: implement a better check for constructor (right now only checks if it has it or not)
-  if (!artifact.functions.find(func => func.name === 'constructor')) {
+  if (!getDefaultInitializer(artifact)) {
     throw new Error('ABI has no constructor');
   }
 

yarn-project/aztec.js/src/contract/contract.test.ts
@@ -121,6 +121,7 @@ describe('Contract Class', () => {
         errorTypes: {},
       },
     ],
+    nonDispatchPublicFunctions: [],
     outputs: {
       structs: {},
       globals: {},

jp4g/aztec-packages-note-discovery

commit 0672160c390fd47941eba590a251aee92fcca483

fix: make playground work with new artifacts (#12713)
Broken after https://github.com/AztecProtocol/aztec-packages/pull/12653

---------

Co-authored-by: MirandaWood <miranda@aztecprotocol.com>

playground/src/components/contract/components/deployContractDialog.tsx
@@ -18,6 +18,7 @@ import {
   type FunctionAbi,
   getDefaultInitializer,
   getInitializer,
+  getAllFunctionAbis,
 } from '@aztec/stdlib/abi';
 import { AztecContext } from '../../../aztecEnv';
 import { FunctionParameter } from '../../common/fnParameter';
@@ -44,10 +45,12 @@ export function DeployContractDialog({
   const [parameters, setParameters] = useState([]);
   const [deploying, setDeploying] = useState(false);
   const { wallet, setLogsOpen } = useContext(AztecContext);
+  const [functionAbis, setFunctionAbis] = useState<FunctionAbi[]>([]);
 
   useEffect(() => {
     const defaultInitializer = getDefaultInitializer(contractArtifact);
     setInitializer(defaultInitializer);
+    setFunctionAbis(getAllFunctionAbis(contractArtifact));
   }, [contractArtifact]);
 
   const handleParameterChange = (index, value) => {
@@ -98,12 +101,12 @@ export function DeployContractDialog({
                 <Select
                   value={initializer?.name ?? ''}
                   label="Initializer"
-                  disabled={!contractArtifact.functions.some(fn => fn.isInitializer)}
+                  disabled={!functionAbis.some(fn => fn.isInitializer)}
                   onChange={e => {
                     setInitializer(getInitializer(contractArtifact, e.target.value));
                   }}
                 >
-                  {contractArtifact.functions
+                  {functionAbis
                     .filter(fn => fn.isInitializer)
                     .map(fn => (
                       <MenuItem key={fn.name} value={fn.name}>

playground/src/components/contract/contract.tsx
@@ -8,7 +8,8 @@ import {
   type ContractArtifact,
   type ContractInstanceWithAddress,
   loadContractArtifact,
-  getContractClassFromArtifact,
+  getAllFunctionAbis,
+  type FunctionAbi,
 } from '@aztec/aztec.js';
 import { AztecContext } from '../../aztecEnv';
 import Button from '@mui/material/Button';
@@ -120,10 +121,11 @@ const loadingArtifactContainer = css({
   gap: '2rem',
 });
 
-const FORBIDDEN_FUNCTIONS = ['process_log', 'sync_notes'];
+const FORBIDDEN_FUNCTIONS = ['process_log', 'sync_notes', 'public_dispatch'];
 
 export function ContractComponent() {
   const [contractArtifact, setContractArtifact] = useState<ContractArtifact | null>(null);
+  const [functionAbis, setFunctionAbis] = useState<FunctionAbi[]>([]);
 
   const [filters, setFilters] = useState({
     searchTerm: '',
@@ -163,11 +165,10 @@ export function ContractComponent() {
       setIsLoadingArtifact(true);
       const artifactAsString = await walletDB.retrieveAlias(`artifacts:${currentContractAddress}`);
       const contractArtifact = loadContractArtifact(parse(convertFromUTF8BufferAsString(artifactAsString)));
-      const contractClass = await getContractClassFromArtifact(contractArtifact);
-      console.log(contractClass);
       const contract = await Contract.at(currentContractAddress, contractArtifact, wallet);
       setCurrentContract(contract);
       setContractArtifact(contract.artifact);
+      setFunctionAbis(getAllFunctionAbis(contract.artifact));
       setFilters({
         searchTerm: '',
         private: true,
@@ -189,6 +190,7 @@ export function ContractComponent() {
       reader.onload = async e => {
         const contractArtifact = loadContractArtifact(JSON.parse(e.target?.result as string));
         setContractArtifact(contractArtifact);
+        setFunctionAbis(getAllFunctionAbis(contractArtifact));
         setIsLoadingArtifact(false);
       };
       reader.readAsText(file);
@@ -430,7 +432,7 @@ export function ContractComponent() {
               </FormGroup>
             </div>
           </div>
-          {contractArtifact.functions
+          {functionAbis
             .filter(
               fn =>
                 !fn.isInternal &&

yarn-project/stdlib/src/abi/contract_artifact.ts
@@ -97,6 +97,17 @@ function isContractArtifact(input: any): input is ContractArtifact {
       return false;
     }
   }
+  if (!Array.isArray(maybeContractArtifact.nonDispatchPublicFunctions)) {
+    return false;
+  }
+  for (const fn of maybeContractArtifact.nonDispatchPublicFunctions) {
+    if (typeof fn.name !== 'string') {
+      return false;
+    }
+    if (typeof fn.functionType !== 'string') {
+      return false;
+    }
+  }
   return true;
 }
 