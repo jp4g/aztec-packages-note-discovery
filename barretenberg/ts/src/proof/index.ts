5f49ef7666baf01126979fc0c5972761987cf67d
iakovenkos <sergey.s.yakovenko@gmail.com>
2026-02-02 11:07:56 +0000

    even more constants

--- barretenberg/ts/src/proof/index.ts
+++ barretenberg/ts/src/proof/index.ts
@@ -9,7 +9,7 @@ export type ProofData = {
   proof: Uint8Array;
 };
 
-export const PAIRING_POINTS_SIZE = 16;
+export const PAIRING_POINTS_SIZE = 8;
 
 // Fields are 32 bytes
 const fieldByteSize = 32;
